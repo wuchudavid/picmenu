9ee422af979e340f51a04b2ea5d8dcaa21f7548e
wuchudavid <david.wu@yamibuy.com>
2025-07-31 22:35:21 -0700

    Fix Next.js config for API routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
